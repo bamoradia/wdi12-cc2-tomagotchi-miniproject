37a652bbbed0b4c6b0af831fa5adf75970c8b0c3
Badal Moradia <badal@i360llc.com>
2018-06-27 14:35:27 -0500

    removed the second interval

--- js/app.js
+++ js/app.js
@@ -74,8 +74,8 @@ $('#feed').on('click', (event) => {
 	pet.feed(); //makes an event listener on the feed button
 });
 $('#sleep').on('click', (event) => {//makes an event listener on the sleep button 
-	if(sleepCondition === false){
-		$('#feed').prop('disabled', true);
+	if(sleepCondition === false){//if previous was not in sleep
+		$('#feed').prop('disabled', true); //disable play and feed buttons
 		$('#feed').addClass('disabled');
 		$('#feed').removeClass('enabled');
 		$('#play').prop('disabled', true);
@@ -83,8 +83,8 @@ $('#sleep').on('click', (event) => {//makes an event listener on the sleep butto
 		$('#play').removeClass('enabled');
 		sleepCondition = true;
 		//$('#feed').css('opacity', .5);
-	} else if(sleepCondition === true){
-		$('#feed').prop('disabled', false);
+	} else if(sleepCondition === true){ //if previous was in sleep
+		$('#feed').prop('disabled', false);//enable feed and play buttons
 		$('#feed').addClass('enabled');
 		$('#feed').removeClass('disabled');
 		$('#play').prop('disabled', false);
@@ -106,75 +106,123 @@ $('#restart').on('click', (event) => {
 
 
 
-
-
-
 const gamePlay = () => { //creates the game play function
 
 	$('#name').text(pet.name)
 
 	const clearId = setInterval( () => { //create an interval timer that increases the age of the pet
-		const dead = checkForDead();
-		pet.ageUp();
-		$($('.stats')[0]).text(`Age: ${pet.age}`);
-		if(pet.age === 1){
-			$('img').attr('src', 'images/baby_tazmanian_devil.png');
-		} else if (pet.age === 5){
-			$('img').attr('src', 'images/tasmanian_devil.gif');
-		}
-		//console.log(pet.age);
-		if(dead) {
-			console.log(`${pet.name} is not aging anymore!`)
-			clearInterval(clearId);
-		}	
-	}, 5000);
 
-	const clearId2 = setInterval( () => { //create an interval timer that increases the other stats of the pet
-
-		if(sleepCondition === true){
-
-			pet.health--;
-			//$($('.stats')[1]).text(`Health: ${pet.health}`);
-			$('.health_meter').css('width', pet.health*10);
-
-			pet.sleep();
-
-			pet.interest--;
-			//$($('.stats')[3]).text(`Interest: ${pet.interest}`);
-			$('.interest_meter').css('width', pet.interest*10);
-		} else if(sleepCondition === false) {
-
-			pet.health--;
-			//$($('.stats')[1]).text(`Health: ${pet.health}`);
-			$('.health_meter').css('width', pet.health*10);
-
-			pet.energy--;
-			//$($('.stats')[2]).text(`Energy: ${pet.energy}`);
-			$('.energy_meter').css('width', pet.energy*10);
-
-			pet.interest--;
-			//$($('.stats')[3]).text(`Interest: ${pet.interest}`);
-			$('.interest_meter').css('width', pet.interest*10);
-		}
-
-			const dead = checkForDead();
-			//console.log(pet.hunger, pet.sleepiness, pet.boredom);
-			if(dead) {
-				console.log(`${pet.name} is dead!`);
-				clearInterval(clearId2);
-				$('img').attr('src', 'images/RIP.png');
+		if(time % 30 === 0){ //every 30 seconds the age goes up
+			pet.ageUp();
+			$($('.stats')[0]).text(`Age: ${pet.age}`); //updates the age on the page
+			if(pet.age === 1){
+				$('img').attr('src', 'images/baby_tazmanian_devil.png'); //changes the image once 1
+			} else if (pet.age === 5){
+				$('img').attr('src', 'images/tasmanian_devil.gif'); //changes the image once 5
 			}
-	}, 5000);
+			//console.log(pet.age);
+			if(checkForDead()) {
+				console.log(`${pet.name} is not aging anymore!`) //checks if pet is dead
+				clearInterval(clearId);
+			}
+		}	
 
-} ;
+		if(time % 10 === 0){//stats go down every 10 seconds
+			if(sleepCondition === true){ //this happens if pet is sleeping
+	
+				pet.health--;
+				//$($('.stats')[1]).text(`Health: ${pet.health}`);
+				$('.health_meter').css('width', pet.health*10); //updates the status bar for health
+	
+				pet.sleep();
+	
+				pet.interest--;
+				//$($('.stats')[3]).text(`Interest: ${pet.interest}`);
+				$('.interest_meter').css('width', pet.interest*10); //updates status bar for interest
+
+			} else if(sleepCondition === false) {//this happens if pet is not sleeping
+	
+				pet.health--;
+				//$($('.stats')[1]).text(`Health: ${pet.health}`);
+				$('.health_meter').css('width', pet.health*10); //updates the status bar for health
+	
+				pet.energy--;
+				//$($('.stats')[2]).text(`Energy: ${pet.energy}`);
+				$('.energy_meter').css('width', pet.energy*10);//updates status bar for energy
+	
+				pet.interest--;
+				//$($('.stats')[3]).text(`Interest: ${pet.interest}`);
+				$('.interest_meter').css('width', pet.interest*10);//updates status bar for interest
+			}
+
+			if(checkForDead()) { //if dead is true
+				console.log(`${pet.name} is dead!`);
+				$('img').attr('src', 'images/RIP.png');//change image
+				clearInterval(clearId);//clear interval
+			}
+		}
+	time++;//increase time
+	}, 1000);
+}
+
+// 	const clearId2 = setInterval( () => { //create an interval timer that increases the other stats of the pet
+
+// 		// if(sleepCondition === true){
+
+// 		// 	pet.health--;
+// 		// 	//$($('.stats')[1]).text(`Health: ${pet.health}`);
+// 		// 	$('.health_meter').css('width', pet.health*10);
+
+// 		// 	pet.sleep();
+
+// 		// 	pet.interest--;
+// 		// 	//$($('.stats')[3]).text(`Interest: ${pet.interest}`);
+// 		// 	$('.interest_meter').css('width', pet.interest*10);
+// 		// } else if(sleepCondition === false) {
+
+// 		// 	pet.health--;
+// 		// 	//$($('.stats')[1]).text(`Health: ${pet.health}`);
+// 		// 	$('.health_meter').css('width', pet.health*10);
+
+// 		// 	pet.energy--;
+// 		// 	//$($('.stats')[2]).text(`Energy: ${pet.energy}`);
+// 		// 	$('.energy_meter').css('width', pet.energy*10);
+
+// 		// 	pet.interest--;
+// 		// 	//$($('.stats')[3]).text(`Interest: ${pet.interest}`);
+// 		// 	$('.interest_meter').css('width', pet.interest*10);
+// 		// }
+
+// 		// 	const dead = checkForDead();
+// 		// 	//console.log(pet.hunger, pet.sleepiness, pet.boredom);
+// 		// 	if(dead) {
+// 		// 		console.log(`${pet.name} is dead!`);
+// 		// 		clearInterval(clearId2);
+// 		// 		$('img').attr('src', 'images/RIP.png');
+// 		// 	}
+// 	}, 5000);
+
+// } ;
 
 
 const checkForDead = () => { //checks if the pet has died
-	if(pet.health <= 0 || pet.energy <= 0 || pet.interest <= 0) {
-		$('#feed').off('click');
+	if(pet.health <= 0 || pet.energy <= 0 || pet.interest <= 0) { //if any stats are 0
+		$('#feed').off('click'); //diables feed, sleep, play and rename buttons
 		$('#sleep').off('click');
 		$('#play').off('click');
 		$('#rename').off('click');
+		$('#feed').prop('disabled', true);
+		$('#feed').addClass('disabled');
+		$('#feed').removeClass('enabled');
+		$('#sleep').prop('disabled', true);
+		$('#sleep').addClass('disabled');
+		$('#sleep').removeClass('enabled');
+		$('#play').prop('disabled', true);
+		$('#play').addClass('disabled');
+		$('#play').removeClass('enabled');
+		$('#rename').prop('disabled', true);
+		$('#rename').addClass('disabled');
+		$('#rename').removeClass('enabled');
 		return true
 	} else {
 		return false
